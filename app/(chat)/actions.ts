a15ae688c012fd219e91b69e867daca26788c5c2
Hugo Paja <hugo.paja05@gmail.com>
2025-09-21 10:27:45 +0200

    title generation prompt tweak

--- app/(chat)/actions.ts
+++ app/(chat)/actions.ts
@@ -34,7 +34,7 @@ export async function generateTitleFromUserMessage({
     system: `\n
     - you will generate a short title based on the first message a user begins a conversation with
     - ensure it is not more than 80 characters long
-    - the title should be a summary of the user's message
+    - the title should be a summary of the user's message but ideally should be no more than 3 words
     - do not use quotes or colons`,
     prompt: JSON.stringify(message),
   });
